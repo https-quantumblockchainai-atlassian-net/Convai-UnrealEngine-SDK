0938a47d99e9321967ccb2149c9eb29db0fca5a4
Crystal_Alchemist <105375894+q1blue@users.noreply.github.com>
2025-02-20 13:56:22 +1100

    Created file with oXygen XML Web Author.

---
+++ Resources/Crystal Ai auto train deep learning multi-modal multi-dimensional vllms-reasoning software-stack wiki.alias.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Aliases for Crystal Ai auto train deep learning multi-modal multi-dimensional vllms-reasoning software-stack wiki extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+	
+];
